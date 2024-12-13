cca81ef153b19e4ae846b1dd375d2f61ef0f777b
abdulrahman <a.majdy41@gmail.com>
2024-12-13 21:00:56 +0200

    added the script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+add_numbers = number1 + number2
+sub_numbers = number1 - number2
+mult_numbers = number2 * number1
+print(f"Addition of {number1} and {number2} is {add_numbers}")
+print(f"Subtraction of {number1} and {number2} is {sub_numbers}")
+print(f"Multiplication of {number1} and {number2} is {mult_numbers}")
