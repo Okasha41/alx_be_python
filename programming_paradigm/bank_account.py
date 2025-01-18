06bd1b7e6e7d66c2c06a2644e0a1df76d1ccdcdf
AbdulRahman Okasha <a.majdy41@gmail.com>
2025-01-18 15:58:13 +0200

    update bank_account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -3,12 +3,12 @@ class BankAccount:
         self.account_balance = balance
 
     def deposit(self, amount):
-        self.account_balance += amount
+        self.account_balance = self.account_balance + amount
         print(f"Deposited: ${amount}")
 
     def withdraw(self, amount):
         if self.account_balance >= amount:
-            self.account_balance -= amount
+            self.account_balance = self.account_balance - amount
             print(f"Withdrew: ${amount}")
             return True
         else:
